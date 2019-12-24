b0c04119eb6c872472ac532a473b851edade33f5
ztysdmy <a.dmytro.tyshchenko@ericsson.com>
2019-12-24 18:00:03 +0200

    Firestore Impl

--- src/main/java/com/giftok/payment/firestore/FirestorePaymentRepository.java
+++ src/main/java/com/giftok/payment/firestore/FirestorePaymentRepository.java
@@ -1,12 +1,40 @@
 package com.giftok.payment.firestore;
 
-public class FirestorePaymentRepository implements PaymentRespository {
-	
-	public FirestorePaymentRepository() {
-	  //  Firestore db = FirestoreOptions.getDefaultInstance().getService();
-	    // [END fs_initialize]
-	   // this.db = db;
+import java.util.HashMap;
+import java.util.function.Function;
 
+import com.giftok.payment.processor.ChargeResponse;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+
+public class FirestorePaymentRepository implements PaymentRespository {
+
+	private final Firestore db;
+
+	private static final String collectionName = "payments-info";
+
+	private final String projectId = "single-outrider-260808";
+
+	public FirestorePaymentRepository() {
+		FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId(projectId)
+				.build();
+		this.db = firestoreOptions.getService();
+
+	}
+
+	public Function<ChargeResponse, Boolean> saveChargeResponse(String certificateId) {
+		return chargeResponse -> {
+			var map = createMap(chargeResponse);
+			db.collection(collectionName).document(certificateId).create(map);
+			return true;
+		};
+	}
+
+	private HashMap<String, Object> createMap(ChargeResponse chargeResponse) {
+		var result = new HashMap<String, Object>();
+		chargeResponse.error().map(error -> result.put("error", error));
+		chargeResponse.paymentId().map(paymentId -> result.put("paymentId", paymentId));
+		return result;
 	}
 
 }
